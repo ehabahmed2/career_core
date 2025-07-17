17398fd840b471a92b55295479c10051bb855f5a
Ehab <ihapbpc@gmail.com>
2025-07-18 01:27:09 +0200

    hidden sc

--- career_mgmnt/settings.py
+++ career_mgmnt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#q*5x1p9jgh#*7grcwxvyzzqto-osg$2@#f2kz*nlfvfp&k@p1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
