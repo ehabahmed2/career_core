40e0f670729f409758cd3513e2a03610256d4ca4
Ehab <ihapbpc@gmail.com>
2025-07-21 00:59:27 +0200

    added trusted leaders imgs, fixed a bug in jobs, replaced state with job offers

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 import datetime
+from jobs.models import Job
 # Create your views here.
 
 def home(request):
     year = datetime.date.today().year
-    return render(request, 'home/main.html',context={'year': year})
+    jobs = Job.objects.all()[:3]
+    return render(request, 'home/main.html',context={'year': year, 'jobs': jobs})
